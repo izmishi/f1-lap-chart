48d6e40af52b4df2c297304ca3257ea82a573617
Izumu Mishima <31067139+izmishi@users.noreply.github.com>
2023-11-29 15:27:50 +0000

    Changed 2023 colours for legibility

--- constructorcolours.py
+++ constructorcolours.py
@@ -9,7 +9,7 @@ colour_for_constructor = {
 		'sauber': '#9B0000',
 		'toro_rosso': '#469BFF',
 		'mclaren': '#FF8700',
-		'williams': '#FFFFFF'
+		'williams': '#FFFFFF',
 	}, 2019: {
 		'alfa': '#9B0000',
 		'ferrari': '#DC0000',
@@ -20,7 +20,7 @@ colour_for_constructor = {
 		'red_bull': '#1E41FF',
 		'renault': '#FFF500',
 		'toro_rosso': '#469BFF',
-		'williams': '#FFFFFF'
+		'williams': '#FFFFFF',
 	}, 2020: {
 		'alfa': '#960000',
 		'alphatauri': '#FFFFFF',
@@ -31,7 +31,7 @@ colour_for_constructor = {
 		'racing_point': '#F596C8',
 		'red_bull': '#0600EF',
 		'renault': '#FFF500',
-		'williams': '#0082FA'
+		'williams': '#0082FA',
 	}, 2021: {
 		'alfa': '#900000',
 		'alphatauri': '#2B4562',
@@ -42,19 +42,8 @@ colour_for_constructor = {
 		'mclaren': '#FF8700',
 		'mercedes': '#00D2BE',
 		'red_bull': '#0600EF',
-		'williams': '#005AFF'
+		'williams': '#005AFF',
 	}, 2022: {
-		'alfa': '#900000',
-		'alphatauri': '#2B4562',
-		'alpine': '#0090FF',
-		'aston_martin': '#006F62',
-		'ferrari': '#DC0000',
-		'haas': '#FFFFFF',
-		'mclaren': '#FF8700',
-		'mercedes': '#00D2BE',
-		'red_bull': '#0600EF',
-		'williams': '#005AFF'
-		# Some of the official colours for 2022 are a bit pale, which makes it harder to disinguish them. So reusing the 2021 colours
 		# 'alfa': '#B12039',
 		# 'alphatauri': '#4E7C9B',
 		# 'alpine': '#2293D1',
@@ -64,17 +53,45 @@ colour_for_constructor = {
 		# 'mclaren': '#F58020',
 		# 'mercedes': '#6CD3BF',
 		# 'red_bull': '#1E5BC6',
-		# 'williams': '#37BEDD'
+		# 'williams': '#37BEDD',
+
+		# Some of the official colours for 2022 are a bit pale, which makes it harder to disinguish them. So reusing the 2021 colours
+		'alfa': '#900000',
+		'alphatauri': '#2B4562',
+		'alpine': '#0090FF',
+		'aston_martin': '#006F62',
+		'ferrari': '#DC0000',
+		'haas': '#FFFFFF',
+		'mclaren': '#FF8700',
+		'mercedes': '#00D2BE',
+		'red_bull': '#0600EF',
+		'williams': '#005AFF',
 	}, 2023: {
-		'alfa': '#C92D4B',
+		# 'alfa': '#C92D4B',
 		'alphatauri': '#5E8FAA',
-		'alpine': '#2293D1',
-		'aston_martin': '#358C75',
-		'ferrari': '#F91536',
-		'haas': '#B6BABD',
-		'mclaren': '#F58020',
+		# 'alpine': '#2293D1',
+		# 'aston_martin': '#358C75',
+		# 'ferrari': '#F91536',
+		# 'haas': '#B6BABD',
+		# 'mclaren': '#F58020',
 		'mercedes': '#6CD3BF',
-		'red_bull': '#3671C6',
-		'williams': '#37BEDD'
+		# 'red_bull': '#3671C6',
+		'williams': '#37BEDD',
+
+		# Some of the official colours for 2023 are a bit pale, which makes it harder to disinguish them. So reusing some of the 2021 colours
+		'alfa': '#900000',
+		# 'alphatauri': '#2B4562',
+		'alpine': '#0090FF',
+		# 'aston_martin': '#006F62',
+		'ferrari': '#DC0000',
+		# 'haas': '#FFFFFF',
+		'mclaren': '#FF8700',
+		# 'mercedes': '#00D2BE',
+		'red_bull': '#0600EF',
+		# 'williams': '#005AFF',
+
+		# Custom colours
+		'aston_martin': '#007C55',
+		'haas': '#dddddd',
 	}, 
 }
