0cff9b17dde28e7e6a7dd72f6f27d711e7f65b54
Izumu Mishima <31067139+izmishi@users.noreply.github.com>
2024-04-04 17:44:34 +0100

    Updated for the 2024 season

--- constructorcolours.py
+++ constructorcolours.py
@@ -93,5 +93,16 @@ colour_for_constructor = {
 		# Custom colours
 		'aston_martin': '#007C55',
 		'haas': '#dddddd',
+	}, 2024: {
+		'alpine': '#FF87BC',
+		'aston_martin': '#229971',
+		'ferrari': '#E8002D',
+		'haas': '#B6BABD',
+		'mclaren': '#FF8000',
+		'mercedes': '#27F4D2',
+		'rb': '#6692FF',
+		'red_bull': '#3671C6',
+		'sauber': '#52E252',	
+		'williams': '#64C4FF',
 	}, 
 }
